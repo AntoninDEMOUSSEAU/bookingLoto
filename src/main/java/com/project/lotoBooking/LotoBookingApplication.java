2c877cc8220c3c8e4f5bf763ca3d9381a1ad4b01
DEMOUSSEAU Antonin <antonin.demousseau@maif.fr>
2023-10-19 16:46:34 +0200

    first commit

---
+++ src/main/java/com/project/lotoBooking/LotoBookingApplication.java
@@ -0,0 +1,13 @@
+package com.project.lotoBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotoBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotoBookingApplication.class, args);
+	}
+
+}
